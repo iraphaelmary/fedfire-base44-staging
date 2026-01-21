9d630d258b932fd991895cda0a7a45243cd5df7c
princeomonu <omonuitanyi@gmail.com>
2026-01-21 11:19:28 +0100

    feat: Introduce FedFire project with updated documentation, Convex integration, and Vite path aliases.

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import base44 from "@base44/vite-plugin"
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -15,5 +15,10 @@ export default defineConfig({
     //   visualEditAgent: true
     // }),
     react(),
-  ]
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
